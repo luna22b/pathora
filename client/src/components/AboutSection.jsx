29b6d07c5942931c331a164ee18884043b9c4bdf
Brandon Luna <brandon.lunaj@gmail.com>
2025-06-16 19:17:36 -0400

    nothing added

--- client/src/components/AboutSection.jsx
+++ client/src/components/AboutSection.jsx
@@ -1,16 +1,16 @@
 const AboutSection = () => {
   const Card = ({ children }) => (
-    <div className="bg-white rounded-2xl w-full h-50 shadow-lg text-center pt-3 px-4">
+    <div className="bg-white rounded-2xl w-full h-60 shadow-lg text-center pt-3 px-4">
       {children}
     </div>
   );
 
   return (
-    <div className="bg-[#fcfcfc] py-16 mt-20">
+    <div className="bg-[#fcfcfc] py-16 lg:mt-35 mt-10">
       <div className="max-w-6xl mx-auto px-4">
-        <div className="flex flex-col gap-6 text-left">
+        <div className="flex flex-col gap-5 text-left">
           <p className="font-semibold text-3xl">Why Choose Pathora?</p>
-          <p className="text-[#545454] text-base">
+          <p className="text-[#545454] text-xl">
             Seamlessly connecting you with the green world around you — here's
             what sets us apart:
           </p>
@@ -23,7 +23,7 @@ const AboutSection = () => {
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                className="size-6 mx-auto mt-2"
+                className="size-10 mx-auto mt-2 bg-[#ddd] p-2 rounded-4xl"
               >
                 <path
                   strokeLinecap="round"
@@ -36,7 +36,9 @@ const AboutSection = () => {
                   d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z"
                 />
               </svg>
-              <p className="pt-2">Take pictures</p>
+              <p className="pt-2 font-semibold text-xl text-black">
+                Take pictures
+              </p>
             </Card>
             <Card>
               <svg
@@ -45,7 +47,7 @@ const AboutSection = () => {
                 viewBox="0 0 24 24"
                 stroke-width="1.5"
                 stroke="currentColor"
-                className="size-6 mx-auto mt-2"
+                className="size-10 mx-auto mt-2 bg-[#ddd] p-2 rounded-4xl"
               >
                 <path
                   stroke-linecap="round"
@@ -53,7 +55,9 @@ const AboutSection = () => {
                   d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
                 />
               </svg>
-              <p className="pt-2">Explore plants</p>
+              <p className="pt-2 font-semibold text-xl text-black">
+                Explore plants
+              </p>
             </Card>
             <Card>
               <svg
@@ -62,7 +66,7 @@ const AboutSection = () => {
                 viewBox="0 0 24 24"
                 stroke-width="1.5"
                 stroke="currentColor"
-                className="size-6 mx-auto mt-2"
+                className="size-10 mx-auto mt-2 bg-[#ddd] p-2 rounded-4xl"
               >
                 <path
                   stroke-linecap="round"
@@ -70,7 +74,9 @@ const AboutSection = () => {
                   d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155"
                 />
               </svg>
-              <p className="pt-2">Talk to AI</p>
+              <p className="pt-2 font-semibold text-xl text-black">
+                Talk to AI
+              </p>
             </Card>
           </div>
         </div>
