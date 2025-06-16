9e3f05e8eb0a54bc064086d799127d1216eab38a
Brandon Luna <brandon.lunaj@gmail.com>
2025-06-16 19:18:02 -0400

    styled the search section - still ongoing!

--- client/src/components/BrowseSearch.jsx
+++ client/src/components/BrowseSearch.jsx
@@ -1,7 +1,23 @@
-const BrowseSearch = () => {
+const BrowseSearch = ({ records = [], totalCount }) => {
   return (
     <div>
-      <p className="flex justify-center mt-5 font-semibold">All Plants</p>
+      <p className="flex justify-center mt-8 font-semibold text-2xl">
+        Plant Database
+      </p>
+      <p className="mt-3 text-[#737373] text-center text-sm w-full px-4">
+        Discover thousands of plant species with comprehensive botanical data
+        powered by the Trefle API.
+      </p>
+      <div className="text-center mt-3 mb-4 text-xs text-[#737373] font-medium">
+        {totalCount.toLocaleString()} species available
+      </div>
+      <div className="flex justify-center w-[90vw] shadow-xs mb-6 h-50 max-w-sm md:max-w-2xl lg:max-w-4xl rounded-lg mx-auto px-4 border border-[#eee]">
+        <input
+          type="text"
+          placeholder="Search Plants..."
+          className="border border-[#ddd] h-10 w-[80vw] rounded-lg bg-[#f5f5f5] pl-12 pr-4 outline-none focus:border-black transition-colors duration-200 mt-4"
+        />
+      </div>
     </div>
   );
 };
