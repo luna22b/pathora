eac4e0a3e85ee6683b79e8ee629d2266b9dd9167
Brandon Luna <brandon.lunaj@gmail.com>
2025-06-18 23:52:27 -0400

    deleted some api requests

--- server/index.js
+++ server/index.js
@@ -13,30 +13,6 @@ app.use(express.static(buildPath));
 
 const TOKEN = process.env.token;
 
-app.get("/api/plants", async (req, res) => {
-  try {
-    const response = await fetch(
-      `https://perenual.com/api/v2/species-list?key=${TOKEN}`
-    );
-
-    if (!response.ok) {
-      const errorBody = await response.json();
-      return res.status(response.status).json({
-        error: "Failed to fetch plant list",
-        details: errorBody,
-      });
-    }
-
-    const data = await response.json();
-    res.json(data);
-  } catch (err) {
-    console.error("Server error:", err);
-    res
-      .status(500)
-      .json({ error: "Error fetching plant list", details: err.message });
-  }
-});
-
 app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(buildPath, "index.html"));
 });
