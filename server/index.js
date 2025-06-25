5134d1170929ba3f8f838ad5716549e14c5b845e
Brandon Luna <brandon.lunaj@gmail.com>
2025-06-25 08:24:58 -0400

    currently adding routes for login and register

--- server/index.js
+++ server/index.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-const fetch = require("node-fetch");
+const authRoutes = require("./routes/auth");
 require("dotenv").config();
-
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 
 const buildPath = path.join(__dirname, "..", "client", "dist");
 app.use(express.static(buildPath));
 
-const TOKEN = process.env.token;
+app.use(authRoutes);
 
-app.get(/(.*)/, (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(buildPath, "index.html"));
 });
 
