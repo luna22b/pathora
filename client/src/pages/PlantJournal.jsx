176adf47071a5b7f143a8b39986a731ec7a953c6
Brandon Luna <brandon.lunaj@gmail.com>
2025-07-01 10:50:02 -0400

    currently working on it. just a journal for the user

--- client/src/pages/PlantJournal.jsx
+++ client/src/pages/PlantJournal.jsx
@@ -1,23 +1,36 @@
+import { useState } from "react";
+
 const PlantJournal = () => {
-  const Card = ({ children }) => (
-    <div className="w-[40vw] rounded-lg shadow-lg h-35"></div>
-  );
+  const [modal, setModal] = useState(false);
 
   return (
-    <div className="font-[Inter]">
-      <div className="font-semibold text-4xl w-full text-center mx-auto mt-8">
-        Where Your <span className="text-[#187C48]">Plants</span> Feel Home
+    <div className="font-[Inter] flex flex-col items-center text-center ">
+      <div className="text-center mt-10 font-bold font-[Inter] text-2xl">
+        Plant Journal
       </div>
-      <p className="mt-5 text-center text-[#737373] text-lg">
-        Track growth, note care, and celebrate every leaf. Your plant's story
-        starts here.
-      </p>
-      <div className="grid grid-cols-2 text-center gap-4 mt-12 place-items-center">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+      <div className="mt-2 text-md w-[80vw] text-[#737373]">
+        Document every step of your plant care journey to see your plants
+        flourish
       </div>
+      <div>
+        <button
+          to="/plants"
+          className="flex items-center justify-center text-[15px] h-11 w-[90vw] rounded-lg bg-green-500 text-white font-semibold shadow-md cursor-pointer mt-6 hover:bg-green-600 transition"
+          onClick={() => setModal(true)}
+        >
+          + New Entry
+        </button>
+      </div>
+
+      {modal && (
+        <div className="absolute inset-0 bg-black/35 backdrop-blur-xs flex items-center justify-center opacity-100">
+          <div className="rounded-lg bg-white shadow-md h-100 w-[90vw]">
+            <div className="flex flex-start ml-5 pt-5 font-semibold">
+              <div>New Journal Entry</div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
