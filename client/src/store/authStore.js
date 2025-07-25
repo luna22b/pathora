d96fcf1929a08ee297fb2214f4cee92ee80ed6c2
Brandon Luna <brandon.lunaj@gmail.com>
2025-07-25 11:55:10 -0400

    changed a bit with the code. mostly unchanged

--- client/src/store/authStore.js
+++ client/src/store/authStore.js
@@ -1,17 +1,19 @@
-import { create } from "zustand";
+import { create } from 'zustand';
 
 const authStore = create((set) => ({
-  user: JSON.parse(localStorage.getItem("user")) || null,
-  token: localStorage.getItem("token"),
-  isAuthenticated: !!localStorage.getItem("token"),
+  user: JSON.parse(localStorage.getItem('user')) || null,
+  token: localStorage.getItem('token'),
+  isAuthenticated: !!localStorage.getItem('token'),
 
   login: (user, token) => {
-    localStorage.setItem("token", token);
+    localStorage.setItem('token', token);
+    localStorage.setItem('user', JSON.stringify(user));
     set({ user, token, isAuthenticated: true });
   },
 
   logout: () => {
-    localStorage.removeItem("token");
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
     set({ user: null, token: null, isAuthenticated: false });
   },
 }));
