2ad8324d61628216077e463410a7aa4cc5a6b874
Brandon Luna <brandon.lunaj@gmail.com>
2025-07-25 12:02:20 -0400

    nothing really changed

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
-const express = require("express");
-const path = require("path");
-const cors = require("cors");
-const authRoutes = require("./routes/auth");
-require("dotenv").config();
+import express from "express";
+import path from "path";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.js";
+import { fileURLToPath } from "url";
+import imageUploadRouter from "./uploads/image-upload.js";
+
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
@@ -13,6 +20,11 @@ const buildPath = path.join(__dirname, "..", "client", "dist");
 app.use(express.static(buildPath));
 
 app.use(authRoutes);
+app.use(imageUploadRouter);
+
+app.get("/test", (req, res) => {
+  res.send("Backend updated version!");
+});
 
 app.get(/.*/, (req, res) => {
   res.sendFile(path.join(buildPath, "index.html"));
