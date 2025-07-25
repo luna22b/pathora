ca5afc598395ddaacdc59e9760fcd5689106bce8
Brandon Luna <brandon.lunaj@gmail.com>
2025-07-25 11:59:55 -0400

    nothing really changed

--- server/middleware/authenticateJWT.js
+++ server/middleware/authenticateJWT.js
@@ -1,15 +1,22 @@
-function authenticateJWT(req, res, next) {
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const authenticateJWT = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "No token provided" });
+  if (authHeader?.startsWith("Bearer ")) {
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, process.env.JWT_TOKEN, (err, user) => {
+      if (err) return res.sendStatus(403);
+      req.user = user;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
   }
+};
 
-  const token = authHeader.split(" ")[1];
-  jwt.verify(token, JWT_TOKEN, (err, user) => {
-    if (err) return res.status(403).json({ message: "Invalid token" });
-
-    req.user = user;
-    next();
-  });
-}
+export default authenticateJWT;
