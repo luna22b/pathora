c20fbd5d8bc8e22af56897a41659517220b7d9fe
Brandon Luna <brandon.lunaj@gmail.com>
2025-07-25 11:58:04 -0400

    changed routing due to nav link change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import PlantJournal from "./pages/PlantJournal";
-import IdentifyPlant from "./pages/IdentifyPlant";
-import NotFound from "./pages/NotFound";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import AllPlants from "./pages/AllPlants";
-import "./App.css";
-import "@fontsource/inter";
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/400-italic.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import PlantJournal from './pages/PlantJournal';
+import IdentifyPlant from './pages/IdentifyPlant';
+import NotFound from './pages/NotFound';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import PlantGallery from './pages/PlantGallery';
+import './App.css';
+import '@fontsource/inter';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/400-italic.css';
 
 function App() {
   return (
@@ -22,7 +22,7 @@ function App() {
         <Route path="/identify" element={<IdentifyPlant />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/plants" element={<AllPlants />} />
+        <Route path="/gallery" element={<PlantGallery />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
